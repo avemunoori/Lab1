1ec91f10d53fe55f130499f71dfaff85da798bc5
Aaryan <avemunoo@charlotte.edu>
2026-02-14 08:28:27 -0500

    Restructure project to match assignment requirements

--- Contracts/IClock.cs
+++ Contracts/IClock.cs
@@ -1,4 +1,4 @@
-namespace Lab1.Domain.Interfaces;
+namespace Lab1.Contracts;
 
 /// <summary>
 /// Provides access to the current date and time.
@@ -10,5 +10,9 @@ public interface IClock
     /// Gets the current date and time.
     /// </summary>
     /// <returns>The current date and time.</returns>
+    /// <remarks>
+    /// Pre-conditions: None
+    /// Post-conditions: Returns current system time or test-configured time
+    /// </remarks>
     DateTime Today();
 }
--- Contracts/IPolicy.cs
+++ Contracts/IPolicy.cs
@@ -1,6 +1,6 @@
 using Lab1.Domain.Models;
 
-namespace Lab1.Domain.Interfaces;
+namespace Lab1.Contracts;
 
 /// <summary>
 /// Defines business rules and policies for library checkout operations.
@@ -13,6 +13,16 @@ public interface IPolicy
     /// <param name="item">The item to check out.</param>
     /// <param name="borrower">The borrower requesting the checkout.</param>
     /// <returns>True if checkout is allowed; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when item or borrower is null.</exception>
+    /// <remarks>
+    /// Pre-conditions: 
+    /// - Item must not be null
+    /// - Borrower must not be null
+    /// 
+    /// Post-conditions: 
+    /// - Returns true if item is available and borrower meets policy requirements
+    /// - Returns false otherwise
+    /// </remarks>
     bool CanCheckout(Item item, Borrower borrower);
 
     /// <summary>
@@ -21,5 +31,9 @@ public interface IPolicy
     /// </summary>
     /// <param name="proposedDate">The proposed due date.</param>
     /// <returns>The normalized due date.</returns>
+    /// <remarks>
+    /// Pre-conditions: proposedDate should be a future date
+    /// Post-conditions: Returns an adjusted date conforming to library policies
+    /// </remarks>
     DateTime NormalizeDueDate(DateTime proposedDate);
 }
