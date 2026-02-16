c0fc0c54a63049a32506e1a9566d89ccb4e65b46
jbotticc <jbotticc@charlotte.edu>
2026-02-15 20:05:09 -0500

    feat: implemented borrower model

--- Domain/Models/Borrower.cs
+++ Domain/Models/Borrower.cs
@@ -2,7 +2,35 @@ namespace Domain.Models;
 
 public class Borrower
 {
-    public int Id { get; init; }
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
+    public int Id { get; }
+    
+    public string Name { get; }
+
+    private string _email;
+    public string Email
+    {
+        get => _email;
+        private set
+        {
+            if (string.IsNullOrWhitespace(value))
+                throw new ArgumentException("Email cannot be empty.", nameof(value));
+            _email = value;
+        }
+    }
+
+    public Borrower(int id, string name, string email)
+    {
+        if (id <= 0)
+            throw new ArgumentException("Id cannot be less than one.", nameof(id));
+        Id = id;
+        if (string.IsNullOrWhitespace(name))
+            throw new ArgumentException("Name cannot be empty.", nameof(name));
+        Name = name;
+        Email = email;
+    }
+
+    public void UpdateEmail(string newEmail)
+    {
+        Email = newEmail;
+    }
 }
